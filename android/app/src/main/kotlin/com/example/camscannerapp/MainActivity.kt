80ec788dcdc5b1c03ada7cc5c5461183fbfd5cb6
EemanNaseer1 <eemannaseer1@gmail.com>
2024-10-02 02:12:20 -0700

    My CamScanner App

---
+++ android/app/src/main/kotlin/com/example/camscannerapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.camscannerapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:camscannerapp/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
